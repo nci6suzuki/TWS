40147cba49942ba4fe56a726edc058b4c5924ce7
suzukimasahiro <m.suzuki@nci6.com>
2026-06-09 11:52:15 +0900

    KEYの名称変更

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export function createSupabaseAdminClient() {
   const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-  const service = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+  const service = process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!;
   return createClient(url, service, {
     auth: { persistSession: false, autoRefreshToken: false },
   });
